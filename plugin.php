0b9006f7f57ab79332bf2309d87f2005ebab479b
HahaIkBenRogier <git@sngrs.com>
2016-06-06 14:55:02 +0200

    Veranderingen

--- plugin.php
+++ plugin.php
@@ -112,17 +112,31 @@ function watf_weight_submit_shortcode() {
 
 function watf_weight_graph_html() {
     
-    echo    ' <canvas id="myChart" width="400" height="400"></canvas>';
+    global $wpdb;
+    $table_weight = $wpdb->prefix . "watf_weight";
+    global $reg_errors;
+    $date = time();
+    $user = get_current_user_id();
+    $result_personal = $wpdb->get_results ("SELECT * FROM ".$table_weight." WHERE `user` = ".$user);
+    
+    foreach($result_personal as $object){
+        echo $object->id."\n";
+        echo $object->date."\n";
+        echo $object->user."\n";
+        echo $object->weight."\n";
+    }
+    print_r($result_personal);
+    
+    echo    '<canvas id="myChart" width="400" height="300"></canvas>';
     echo    '<script>
                 var ctx = document.getElementById("myChart");
                 var myChart = new Chart(ctx, {
                     type: "line",
                     data: {
-                        labels: ["Red", "Red", "Yellow", "Green", "Purple", "Orange"],
+                        labels: ["Totaal gemiddelde", "Dit jaar", "Vorige maand", "Deze maand", "Vorig jaar", "Deze week"],
                         datasets: [
                             {
-                                label: "Jouw",
-                                backgroundColor: "rgba(255, 139, 46, 0.4)",
+                                label: "Amstel 115",
                                 borderColor: "rgba(255, 139, 46, 1)",
                                 borderCapStyle: "butt",
                                 borderDash: [],
@@ -138,10 +152,9 @@ function watf_weight_graph_html() {
                                 pointRadius: 1,
                                 pointHitRadius: 10,
                                 data: [36, 30, 31.5, 30, 25, 21]
-                            },
+                            } /**,
                              {
-                                label: "Buurt",
-                                backgroundColor: "rgba(23, 198, 155, 0.4)",
+                                label: "De Horden",
                                 borderColor: "rgba(23, 198, 155, 1)",
                                 borderCapStyle: "butt",
                                 borderDash: [],
@@ -159,8 +172,7 @@ function watf_weight_graph_html() {
                                 data: [40, 36, 37, 31, 27, 25]
                             },
                             {
-                                label: "Stad",
-                                backgroundColor: "rgba(23, 198, 155, 0.4)",
+                                label: "Wijk bij Duurstede",
                                 borderColor: "rgba(23, 198, 155, 1)",
                                 borderCapStyle: "butt",
                                 borderDash: [],
@@ -178,8 +190,7 @@ function watf_weight_graph_html() {
                                 data: [35, 30, 32, 31, 29, 30]
                             },
                             {
-                                label: "Provincie",
-                                backgroundColor: "rgba(23, 198, 155, 0.4)",
+                                label: "Provincie Utrecht",
                                 borderColor: "rgba(23, 198, 155, 1)",
                                 borderCapStyle: "butt",
                                 borderDash: [],
@@ -198,7 +209,6 @@ function watf_weight_graph_html() {
                             },
                             {
                                 label: "Nederland",
-                                backgroundColor: "rgba(49, 49, 49, 0.4)",
                                 borderColor: "rgba(49, 49, 49, 1)",
                                 borderCapStyle: "butt",
                                 borderDash: [],
@@ -214,7 +224,7 @@ function watf_weight_graph_html() {
                                 pointRadius: 1,
                                 pointHitRadius: 10,
                                 data: [12, 19, 3, 5, 2, 3]
-                            }
+                            } **/
                         ]
                     },
                     options: {
