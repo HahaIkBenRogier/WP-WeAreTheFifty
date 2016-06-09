aeecdce7011a1f6c07010a2014f0ac8ae7b564a4
HahaIkBenRogier <git@sngrs.com>
2016-06-09 15:05:08 +0200

    SQL tabel voor profielen aanmaken

--- index.php
+++ index.php
@@ -5,9 +5,71 @@
 * Author: Rogier Sangers
 **/
 
-include_once( plugin_dir_path( __FILE__ ) . 'install.php' );
 include_once( plugin_dir_path( __FILE__ ) . '/weight/form.php' );
 include_once( plugin_dir_path( __FILE__ ) . '/weight/graph.php' );
+
+// Installeer tabel
+function watf_weight_install_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "watf_weight";
+        if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+          $sql = "CREATE TABLE " . $table_name . " (
+          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+          `date` int(11) NOT NULL,
+          `user` int(11) NOT NULL,
+          `weight` int(11) NOT NULL,
+          `earned_points` int(11) NOT NULL,
+          `current_points` int(11) NOT NULL,
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `user` (`user`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+          require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+          dbDelta($sql);
+    }
+}
+register_activation_hook(__FILE__, 'watf_weight_install_table');
+
+function watf_weight_install_table2() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "watf_points";
+        if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+          $sql = "CREATE TABLE " . $table_name . " (
+            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+            `user` int(11) DEFAULT NULL,
+            `points` int(11) DEFAULT NULL,
+            `lastlogin` int(11) DEFAULT NULL,
+            PRIMARY KEY (`id`)
+          );";
+          require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+          dbDelta($sql);
+    }
+}
+register_activation_hook(__FILE__, 'watf_weight_install_table2');
+
+function watf_weight_install_table3() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "watf_users";
+        if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+          $sql = "CREATE TABLE " . $table_name . " (
+          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+          `user` int(11) NOT NULL,
+          `Street` varchar(255) NOT NULL DEFAULT '',
+          `AdressNumber` varchar(255) NOT NULL DEFAULT '',
+          `PostalCode` varchar(6) NOT NULL DEFAULT '',
+          `Neighbourhood` varchar(255) DEFAULT NULL,
+          `City` varchar(255) DEFAULT NULL,
+          `State` varchar(255) DEFAULT NULL,
+          `HousingGroup` varchar(255) NOT NULL DEFAULT '',
+          `registered_on` int(11) NOT NULL,
+          `activated` tinyint(1) DEFAULT NULL,
+          `activated_on` int(11) DEFAULT NULL,
+          PRIMARY KEY (`id`)
+          ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;";
+          require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+          dbDelta($sql);
+    }
+}
+register_activation_hook(__FILE__, 'watf_weight_install_table3');
 
 add_action( 'wp_enqueue_scripts', 'watf_add_styles_scripts' );
 function watf_add_styles_scripts() {
